b6ae365cc05d520e23acbbd0d336a6fe4b6d13ca
Sung Ho Hwang <sgc109109@gmail.com>
2018-12-25 01:09:14 +0900

    FC submitted

--- quora_insincere_questions_classification/kaggle_train.py
+++ quora_insincere_questions_classification/kaggle_train.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+import os
 from keras import layers, models
 from sklearn.model_selection import train_test_split
 from keras_preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 from keras.optimizers import Adam
 from sklearn import metrics
+
+embed_size = 300
+max_features = 50000
+max_len = 100
+fname_model = 'trained_model.h5'
 
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
@@ -14,10 +20,6 @@ print('train shape : ', train_df.shape)
 print('test shape : ', test_df.shape)
 
 train_df, val_df = train_test_split(train_df, test_size=0.1, random_state=2018)
-
-embed_size = 300
-max_features = 50000
-max_len = 100
 
 train_x = train_df['question_text'].fillna('__na__').values
 val_x = val_df['question_text'].fillna('__na__').values
@@ -38,16 +40,29 @@ train_y = train_df['target'].values
 val_y = val_df['target'].values
 
 model = models.Sequential()
-model.add(layers.Dense(512, activation='relu', input_shape=(max_len,)))
+
+# if os.path.isfile(fname_model):
+#     model = models.load_model(fname_model)
+# else:
+model.add(layers.Embedding(max_features, embed_size, input_length=max_len))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(train_x, train_y, batch_size=512, epochs=2, validation_data=(val_x, val_y))
+model.save(fname_model)
 
-pred_test = model.predict(test_x, batch_size=512)
+print(model.summary())
+
+pred_val = model.predict([val_x], batch_size=512)
 
 for thresh in np.arange(0.1, 0.501, 0.01):
     thresh = np.round(thresh, 2)
     print("F1 score at threshold {0} is {1}".format(thresh, metrics.f1_score(val_y, (pred_test > thresh).astype(int))))
 
-# pred_test = (pred_test > 0.5).astype(int)
-# out_df =
+pred_test = model.predict([test_x], batch_size=512)
+
+pred_test = (pred_test>0.33).astype(int)
+out_df = pd.DataFrame({"qid":test_df["qid"].values})
+out_df['prediction'] = pred_test
+out_df.to_csv("submission.csv", index=False)
